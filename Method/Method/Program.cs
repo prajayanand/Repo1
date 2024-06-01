dc27de505786f90a4fb388bfc634cd06fe862a6a
prajayanand <PRAJAYANAND@GMAIL.COM>
2024-06-01 00:43:24 -0500

    Add files via upload

---
+++ Method/Method/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = GenerateNumbers(10);
+            Reverse(numbers);
+            PrintNumbers(numbers);
+        }
+
+        // Generate
+        static int[] GenerateNumbers(int length)
+        {
+            int[] numbers = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            return numbers;
+        }
+
+        // Reverse
+        static void Reverse(int[] numbers)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
+            {
+                int temp = numbers[left];
+                numbers[left] = numbers[right];
+                numbers[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        // print
+        static void PrintNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}    
---
+++ Method/Method2/Fib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Method2
+{
+    class Fib
+    {
+        static void Main(string[] args)
+        {
+            // Loop through the first 10 numbers of the Fibonacci sequence and print them
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}");
+            }
+        }
+
+        // Method to calculate the nth Fibonacci number using recursion
+        static int Fibonacci(int n)
+        {
+            // Base case: the first and second Fibonacci numbers are 1
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+        
+            // Recursive case: the nth Fibonacci number is the sum of the (n-1)th and (n-2)th numbers
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+
+    
+}
